1a2b9a3d9cd7b20151d0ba792c54601bb3309420
putrapang <pangestuput20@gmail.com>
2023-07-13 23:44:33 +0700

    menambahkan file login dan css

--- login.php
+++ login.php
@@ -1,31 +1,64 @@
 <?php
-include 'functions.php';
+// Konfigurasi database
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'phpcrud';
+
+// Membuat koneksi ke database
+$koneksi = mysqli_connect($host, $username, $password, $database);
+
+// Memeriksa apakah koneksi berhasil
+if (!$koneksi) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+
+// Memeriksa apakah form login sudah dikirimkan
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Melakukan query untuk memeriksa keberadaan pengguna dengan username dan password yang sesuai
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($koneksi, $query);
+
+    // Memeriksa jumlah baris hasil query
+    if (mysqli_num_rows($result) == 1) {
+        // Login berhasil, arahkan pengguna ke halaman selamat datang
+        header("Location: index.php");
+        exit();
+    } else {
+        // Login gagal, tampilkan pesan kesalahan
+        $error = "Username atau password salah.";
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login</title>
-	<link rel="stylesheet" type="text/css" href="login.css">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style_login.css">
 </head>
-<body> 
-	<div class="kotak_login">
-		<p class="tulisan_login">Login Admin</p>
- 
-		<form action="index.php">
-			<label>Username</label>
-			<input type="text" name="username" class="form_login" placeholder="Username">
- 
-			<label>Password</label>
-			<input type="text" name="password" class="form_login" placeholder="Password">
- 
-			<input type="submit" class="tombol_login" value="LOGIN">
-		</form>
-		
-	</div>
- 
- 
+<body>
+    <div class="container">
+        <h2>Login</h2>
+        <form method="POST" action="">
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" name="password" required>
+            </div>
+            <div class="form-group">
+                <input type="submit" name="submit" value="Login">
+            </div>
+        </form>
+        <?php if (isset($error)) { ?>
+            <p class="error"><?php echo $error; ?></p>
+        <?php } ?>
+    </div>
 </body>
 </html>
-
-
-
